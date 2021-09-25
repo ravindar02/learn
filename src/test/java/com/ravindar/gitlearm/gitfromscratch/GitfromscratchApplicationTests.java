b4d8dae7470b9d1ca468c2b56a57cf17f8a205ce
ravindar <ravindardayachand@gmail.com>
2021-09-25 19:33:23 +0530

    Commit again

---
+++ src/test/java/com/ravindar/gitlearm/gitfromscratch/GitfromscratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravindar.gitlearm.gitfromscratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitfromscratchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
